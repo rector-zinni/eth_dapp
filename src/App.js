ab9c8ee7b19e004f0c1e44115062a06f8eed9e0e
showole sheriff <showolesheriff7@gmail.com>
2024-05-10 21:18:59 -1200

    the revamp version of zinni dapp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import WalletValidation from './views/WalletValidation';
+
+function App() {
+  return (
+    <div className="">
+    
+
+    
+    <WalletValidation/>
+    </div>
+  );
+}
+
+export default App;
